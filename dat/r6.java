5c7f96173f9b86a587eb27214c9639515f890b62
Enej Peternel <enej.peternel@scng.si>
2026-09-17 13:16:39 +0200

    fw

--- dat/r6.java
+++ dat/r6.java
@@ -4,8 +4,25 @@ import java.io.*;
 
 public class r6 {
     public static void main(String[] args) throws IOException {
-        PrintWriter pw= new PrintWriter("C:\\ijoz\\dat\\A.txt");
-        pw.println("feuwihfiew");
-        pw.close();
+        BufferedReader br=new BufferedReader(new FileReader("C:\\ijoz\\dat\\clanek.txt"));
+        String vrsica=br.readLine();
+        int st=0;
+        while (vrsica!=null) {
+            int zac=0;
+            int p=vrsica.indexOf(' ');
+            while (p!=-1) {
+                String beseda = vrsica.substring(zac,p);
+                if (beseda.equalsIgnoreCase("danes")) {
+                    st++;
+                }
+                zac=p+1;
+                p=vrsica.indexOf(' ',zac);
+                
+            }
+            vrsica=br.readLine();
+            
+        }
+        System.out.println("Beseda danes se pojavi "+st+" krat.");
+        br.close();
     }
 }
